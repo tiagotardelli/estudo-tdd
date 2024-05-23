0f9e3ccea4b0b9f69053866793bc925f1b15c8c6
Tiago Tardelli <tiagob.tardelli@gmail.com>
2024-05-22 21:44:13 -0300

    Teste funcional agora verifica se podemos inserir um item de tarefa a fazer.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,33 +1,58 @@
-from selenium import webdriver
+"""Testes funcionais usando Selenium"""
+import time
 import unittest
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 class NewVisitorTest(unittest.TestCase):
+    """Classe de testes funcionais"""
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDiwb(self):
+    def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_reatrive_it_later(self):
-        # Edith ouviu falar de uma nova aplicação online interessante para lista
-        # de tarefas. Ela decide verificar sua homepage
+        """Testes com as listas"""
+
+        # Edith ouviu falar de uma nova aplicação online interessante para lista de tarefas.
+        # Ela decide verificar sua homepage
         self.browser.get('http://localhost:8000')
 
-        # Ela percebe que o título da página e o cabeçalho menionam listas de tarefas (to-do)
+        # Ela percebe que o título da página e o cabeçalho mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a inserir um item de tarefa imediatamente
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
+        # de texto (o hobby de Edith é fazer iscas para pescar com fly)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # Quando ela tecla enter, a página é atualizada, e agora a página lista
+        # 1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers'  for row in rows)
+        )
+
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
+        # Ela insere "Use peacock feathers to make fly (Usar penas de pavão para fazer
+        # um fly - Edith é bem metódica)
         self.fail('Finish the test!')
-
-    # Ela é convidada a inserir um item de tarefa imediatamente
-    # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
-    # de texto (o hobby de Edith é fazer iscas para pescar com fly)
-
-    # Quando ela tecla enter, a página é atualizada, e agora a página lista
-    # "1: Buy peacock feathers" como um item em uma lista de tarefas
-
-    # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
-    # Ela insere "Use peacock feathers to make fly (Usar penas de pavão para fazer
-    # um fly - Edith é bem metódica)
 
     # A página é atualizada novamente e agora mostra os dois itens em sua lista
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,5 @@
+"""Módulo para testes unitários"""
+
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -5,12 +7,16 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+    """Class representando testes"""
 
     def test_root_url_resolves_to_home_page_view(self):
+        """Teste de url para verificar o endereço da home page"""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-    
+
+
     def test_home_page_returns_correct_html(self):
+        """Teste para verificar se é encontrada a informação na página"""
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
