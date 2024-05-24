eb4b2bd271ecb21d7daa5f361abe7e03a36ff5e3
Tiago Tardelli <tiagob.tardelli@gmail.com>
2024-05-23 23:59:05 -0300

    Feita refatoração na tratativa dos testes de lista

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_element(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_reatrive_it_later(self):
         """Testes com as listas"""
 
@@ -42,10 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # 1: Buy peacock feathers" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
         # Ela insere "Use peacock feathers to make fly (Usar penas de pavão para fazer
@@ -56,10 +58,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site
         # gerou uma URL único para ela -- há um pequeno texto explicativo para isso.
