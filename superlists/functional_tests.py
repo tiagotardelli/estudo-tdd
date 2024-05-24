cd55cfba6e7b1b758b846accedd670b1dacde6bc
Tiago Tardelli <tiagob.tardelli@gmail.com>
2024-05-23 23:42:14 -0300

    Alterações feitas para validar o primeiro item da lista e o segundo

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -44,24 +44,29 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not apper in table"
-        )
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
         # Ela insere "Use peacock feathers to make fly (Usar penas de pavão para fazer
         # um fly - Edith é bem metódica)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página é atualizada novamente e agora mostra os dois itens em sua lista
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site
+        # gerou uma URL único para ela -- há um pequeno texto explicativo para isso.
         self.fail('Finish the test!')
 
-    # A página é atualizada novamente e agora mostra os dois itens em sua lista
+        # Ela acessa essa URL - sua lista de tarefas continua lá.
 
-    # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site
-    # gerou uma URL único para ela -- há um pequeno texto explicativo para isso.
-
-    # Ela acessa essa URL - sua lista de tarefas continua lá.
-
-    # Satisfeita, ela volta a dormir
+        # Satisfeita, ela volta a dormir
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
